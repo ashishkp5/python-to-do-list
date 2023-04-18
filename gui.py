3a48a0f986c541149e8c8c88508f6a697c48b8e6
Ashish Kumar <ashish-j.kumar@db.com>
2023-04-18 19:09:14 +0530

    Adding Edit feature in todo list

--- gui.py
+++ gui.py
@@ -3,15 +3,19 @@ import PySimpleGUI as sg
 
 lable = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
-button = sg.Button("Add")
-
+add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
 window = sg.Window('My To-Do App',
-                   layout=[[lable], [input_box], [button]],
+                   layout=[[lable], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
+
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
     match event:
         case "Add":
             todos = functions.get_todos("todos.txt")
@@ -19,6 +23,18 @@ while True:
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=functions.get_todos())
+        case "Edit":
+            todo_to_edited = values["todos"][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edited)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value=new_todo)
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
