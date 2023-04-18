752b7b8ebd7eaa91463bfc66e05873924bc1bf29
Ashish Kumar <ashish-j.kumar@db.com>
2023-04-18 14:47:25 +0530

    Day 15, SimpleGUI for gui

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ lable = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo")
 button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[lable, input_box, button]])
+window = sg.Window('My To-Do App', layout=[[lable], [input_box], [button]])
 print("Hello")
 window.read()
 window.close()
